51df4ede81da8bda86608dff94a4be881671f85b
charlytron <ctompkins32@gmail.com>
2022-08-30 19:54:07 -0500

    read...while iterations

--- scripts/read_while.sh
+++ scripts/read_while.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+# Iterating through a file line by line
+
+# while read line; do
+#     echo $line
+# done < "$1"
+
+# process substitution
+
+# Iterating over the ls command output
+# as if it were a file
+
+while read line; do
+  echo "$line"
+done < <(ls $HOME)
